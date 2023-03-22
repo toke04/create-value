958dba1bc814dc4d50e35be66f4f34fc732fca33
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-22 18:33:08 +0900

    READMEの変更 + prettierの指摘を反映

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # 概要
 
-本プログラムは、手軽に  
+本プログラムは手軽に  
 ・配列  
 ・オブジェクト(rubyではハッシュとしても使えます)  
 を作成する事が出来ます。  
-  
-オプションを指定してご利用下さい。  
 
-# How to use this npm package
+プログラミング学習で必要なデータを、手軽に作成する事が出来ます。
+  
+オプションを指定してご利用下さい。
+
+# ご利用方法
 
 ```
-npm install -g valop
-npx valop
+# インストール
+npm install -g valueop
+
+# 使用例
+❯ valueop -a neko inu salu 
+[ 'neko', 'inu', 'salu' ]
+
+❯ valueop -r 1..5
+[ 1, 2, 3, 4, 5 ]
+
+❯ valueop -r a..e 
+[ 'a', 'b', 'c', 'd', 'e' ]
+
+❯ valueop -o neko inu salu 
+{ key1: 'neko', key2: 'inu', key3: 'salu' }
 ```
--- index.js
+++ index.js
@@ -1,31 +1,41 @@
+#! /usr/bin/env node
 const argv = require("minimist")(process.argv.slice(2));
 
 if (argv.a) {
   const array = [argv.a].concat(argv._);
   console.log(array);
 } else if (argv.r) {
-  const ranges = argv.r.match( /\w+/g );
-  const convertedArray = rangeToArray(ranges)
-  console.log(convertedArray)
+  const ranges = argv.r.match(/\w+/g);
+  const convertedArray = rangeToArray(ranges);
+  console.log(convertedArray);
 } else if (argv.o) {
   const array = [argv.o].concat(argv._);
   let returnedObject = {};
 
   for (let index = 0; index < array.length; index++) {
-    returnedObject['key' + (index + 1)] = array[index];
+    returnedObject["key" + (index + 1)] = array[index];
   }
 
   console.log(returnedObject);
-} else{
+} else {
   const dolphinBeforeStdin = `
-本プログラムは、手軽に
-・配列
-・オブジェクト(rubyではハッシュとしても使えます)
-を作成する事が出来ます。
-
 オプションを指定してご利用下さい。
-例
--a : inu neko salu kiji
+[例]
+-a : スペース区切りで値を複数渡すと、配列が出来ます
+❯ valueop -a neko inu salu
+[ 'neko', 'inu', 'salu' ]
+
+-r : 1. [1以上の数値]..[2以上の値]を渡す
+   : 2. [a以上の値]..[z以下の値]を渡す
+❯ valueop -r 1..5         
+[ 1, 2, 3, 4, 5 ]
+
+❯ valueop -r a..e
+[ 'a', 'b', 'c', 'd', 'e' ]
+
+-o : スペース区切りで値を複数渡すと、オブジェクトが作成されます
+❯ valueop -o neko inu salu
+{ key1: 'neko', key2: 'inu', key3: 'salu' }
 `;
   console.log(dolphinBeforeStdin);
 }
@@ -36,8 +46,12 @@ function rangeToArray(argv) {
     for (let index = Number(argv[0]); index <= Number(argv[1]); index++) {
       returnedArray.push(index);
     }
-  }else{
-    for(let index = argv[0].charCodeAt(0); index <= argv[1].charCodeAt(0); index++) {
+  } else {
+    for (
+      let index = argv[0].charCodeAt(0);
+      index <= argv[1].charCodeAt(0);
+      index++
+    ) {
       returnedArray.push(String.fromCharCode(index));
     }
   }
